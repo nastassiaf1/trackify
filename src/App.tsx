47a1e73f1cdbaf35e6db7356bbce019477e92ce5
Anastasiya Fedotova <a.fedotova@itransition.com>
2025-04-13 20:16:25 +0400

    rename about page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import HomePage from './pages/home-page';
 import Navigation from './components/navigation';
 import { AuthProvider } from './context/auth-context';
 import { NotificationProvider } from './context/notification-context';
-import AboutPage from './pages/about/index-page';
+import AboutPage from './pages/about';
 import ContactsPage from './pages/contacts/contacts-page';
 import ProfilePage from './pages/profile/profile-page';
 import BoardPage from './pages/profile/board/board-page';
